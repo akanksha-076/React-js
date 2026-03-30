fa12f148d4f9f1109ec185083d278190efdffab7
Akanksha <a31695132@gmail.com>
2026-03-30 15:38:29 +0530

    REACT JS

---
+++ vite project/README.md
@@ -0,0 +1,13 @@
+# Note
+
+1. Frist letter of the component must be capital
+2. Diffing algo and reconciliation
+3. only one comopnent(function) allowed to be default export
+4. Wrap named export components in { }
+
+```
+const Demo=()=>{
+    return "hello world"
+}
+export {Demo}
+```
---
+++ vite project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import { subtract ,add} from './assets/components/add'
+
+function App() {
+  console.log(subtract(5,2));
+  console.log(add(10,3));
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
